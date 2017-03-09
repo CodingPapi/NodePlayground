b3927780d2b62c73bb87b5c502938b3bf913151f
CodingPapi <codingpapi@gmail.com>
2017-03-09 11:17:56 +0800

    Taste Webpack

--- src/server/index.js
+++ src/server/index.js
@@ -14,10 +14,11 @@ app.use(STATIC_PATH, express.static('dist'))
 app.use(STATIC_PATH, express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send(renderApp(APP_NAME))
+    res.send(renderApp(APP_NAME))
 })
 
 app.listen(WEB_PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(development)'}.`)
+    // eslint-disable-next-line no-console
+    console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
+            '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
 })
